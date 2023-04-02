37aa1b0741f1213465ba786cacdc7e210f741ec8
propscode <mrn00@hotmail.com>
2023-04-02 13:16:01 +0200

    git push

--- script.js
+++ script.js
@@ -19,12 +19,15 @@ function toggleUnderline() {
     el.classList.toggle("underline");
 }
 
-
 function toggleBold() {
-    el.classList.toggle("bold")
+    el.classList.toggle("bold");
 }
 function toggleItalic() {
-    el.classList.toggle("italic")
+    el.classList.toggle("italic");
 }
-function addMonospace() {}
+
+
+function addMonospace() {
+    el.classList.add("gray-text", "mono")
+}
 function removeFormat() {}
