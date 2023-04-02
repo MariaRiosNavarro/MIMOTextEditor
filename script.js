f421b4dcbc1627b28c971d6b85d6533c742a38b2
propscode <mrn00@hotmail.com>
2023-04-02 20:26:38 +0200

    addEventListener Comment added

--- script.js
+++ script.js
@@ -57,4 +57,19 @@ selectElement.addEventListener('change', function(){
     link.src = this.value;
 })
 
+/** es gibt viele Ereignisse, die man mit der .addEventListener() Methode abfangen kann. 
+ Hier sind einige der wichtigsten Ereignisse, die oft verwendet werden:
+'click': tritt auf, wenn das Element mit der Maus geklickt wird
+'mouseover': tritt auf, wenn die Maus über das Element bewegt wird
+'mouseout': tritt auf, wenn die Maus das Element verlässt
+'keydown': tritt auf, wenn eine Taste auf der Tastatur gedrückt wird
+'keyup': tritt auf, wenn eine Taste auf der Tastatur losgelassen wird
+'submit': tritt auf, wenn ein Formular abgeschickt wird
+'change': tritt auf, wenn der Wert eines Formularelements geändert wird (z.B. durch Auswahl eines neuen Elements in einem Dropdown-Menü oder durch Aktivieren eines Kontrollkästchens)
+'resize': tritt auf, wenn das Fenster oder das Element selbst in der Größe geändert wird
+'scroll': tritt auf, wenn der Benutzer auf einer Webseite scrollt
+'contextmenu': tritt auf, wenn der Benutzer mit der rechten Maustaste auf das Element klickt
+Es gibt noch viele weitere Ereignisse, die abgefangen werden können. 
+Eine vollständige Liste der Ereignisse findest du 
+in der offiziellen Dokumentation von Mozilla: https://developer.mozilla.org/en-US/docs/Web/Events */
 
